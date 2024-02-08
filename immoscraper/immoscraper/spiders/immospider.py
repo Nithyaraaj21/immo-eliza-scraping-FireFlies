f4804b6582d3ba1952a75db028e6178cbd25ad6a
Polina.Ya <polinaya777@gmail.com>
2024-02-08 20:12:46 +0100

    delete dataframe, add main.py, save to csv, check consistency

--- immoscraper/immoscraper/spiders/immospider.py
+++ immoscraper/immoscraper/spiders/immospider.py
@@ -11,21 +11,7 @@ class ImmospiderSpider(scrapy.Spider):
 
     # List to store all URLs of the propeties to prevent duplicates
     urls = []
-    # Dictionary to store the data of each property
-    data_dict = {}
-    #df = pd.DataFrame()
-
-    with open("all_data.csv", 'w', newline='', encoding='utf-8') as f:
-            writer = csv.writer(f)
-            fieldnames = [
-                "URL", "ID", "Type of Sale", "Year of Construction", "Price", "Location", "Type", "Subtype", "Bedrooms",
-                "Land Surface", "Kitchen Type", "Fireplace Exists", "Has Terrace",
-                "Terrace Surface", "Has Garden", "Garden Surface", "Net Habitable Surface",
-                "Facade Count", "Has Swimming Pool", "Is Newly Built",
-                "Number of Rooms", "Area", "Kitchen", "Furnished", "Fireplace",
-                "Terrace Area", "Garden Area", "Land", "Number of Facades", "Pool", "State"
-            ]
-            writer.writerow(fieldnames)
+    data_counter = 0
 
     def start_requests(self):
         '''Function to start the scraping process.'''
@@ -44,11 +30,9 @@ class ImmospiderSpider(scrapy.Spider):
                     # Construct the URL to send the request to
                     url = f'{base_url}{house_endpoint}{for_sale_endpoint}{country_endpoint}{life_annuity_endpoint}{public_sale_endpoint}{order_by_endpoint}&page={page}'
                     # Send a request to the URL and call the parse function
-                    yield scrapy.Request(url, callback=self.parse)
-        
-        # print(ImmospiderSpider.urls, len(ImmospiderSpider.urls))
-        #print(ImmospiderSpider.pd)
-
+                    yield scrapy.Request(url, callback=self.parse)        
+    
+    
     def parse(self, response):
         '''Function to parse the response and extract the URLs of the properties.'''
         r_urls = response.xpath('//div[@class="card--result__body"]//a[@class="card__title-link"]/@href').getall()
@@ -66,9 +50,44 @@ class ImmospiderSpider(scrapy.Spider):
         #print(r_details)
         soup = BeautifulSoup(r_details, 'html.parser')
         property_data = soup.text.strip().replace('window.classified = ', '').replace(';', '')
-        property_dict : dict = json.loads(property_data)
-        df = pd.DataFrame.from_dict(property_dict, orient='index')
-        df.to_csv('out.csv', index=False)
-        # Create a PropertyListing object
-        #listing = PropertyListing(property_dict)
+        property_dict = json.loads(property_data)
+
+        # Extracting necessary fields from the property dictionary
+        row = [
+            response.url,
+            property_dict.get('id', ''),
+            property_dict.get('price', {}).get('mainValue', ''),
+            property_dict.get('property', {}).get('location', {}).get('postalCode', '') + '/' + property_dict.get('property', {}).get('location', {}).get('locality', ''),
+            property_dict.get('property', {}).get('type', ''),
+            property_dict.get('property', {}).get('subtype', ''),
+            property_dict.get('property', {}).get('bedroomCount', ''),
+            property_dict.get('property', {}).get('land', {}).get('surface', ''),
+            property_dict.get('property', {}).get('kitchen', {}).get('type', ''),
+            property_dict.get('property', {}).get('fireplaceExists', ''),
+            property_dict.get('property', {}).get('hasTerrace', ''),
+            property_dict.get('property', {}).get('terraceSurface', ''),
+            property_dict.get('property', {}).get('hasGarden', ''),
+            property_dict.get('property', {}).get('gardenSurface', ''),
+            property_dict.get('property', {}).get('netHabitableSurface', ''),
+            property_dict.get('property', {}).get('building', {}).get('facadeCount', ''),
+            property_dict.get('property', {}).get('hasSwimmingPool', ''),
+            property_dict.get('flags', {}).get('isNewlyBuilt', ''),
+            property_dict.get('property', {}).get('roomCount', ''),
+            property_dict.get('property', {}).get('area', ''),
+            property_dict.get('property', {}).get('kitchen', ''),
+            property_dict.get('property', {}).get('furnished', ''),
+            property_dict.get('property', {}).get('fireplace', ''),
+            property_dict.get('property', {}).get('terraceArea', ''),
+            property_dict.get('property', {}).get('gardenArea', ''),
+            property_dict.get('property', {}).get('land', ''),
+            property_dict.get('property', {}).get('building', {}).get('numberOfFacades', ''),
+            property_dict.get('property', {}).get('building', {}).get('pool', ''),
+            property_dict.get('property', {}).get('building', {}).get('state', '')
+        ]
+        
+        # Append the row to the CSV file
+        with open("all_data.csv", 'a', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerow(row)
+            ImmospiderSpider.data_counter += 1
         
